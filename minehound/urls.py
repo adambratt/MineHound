f03688cd4343ce49d5a51aff8b76a6ed0eb1e561
Adam Bratt <adamjbratt@gmail.com>
2012-03-24 18:00:39 -0400

    updated some pages

--- minehound/urls.py
+++ minehound/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'minehound.views.home', name='home'),
-    # url(r'^minehound/', include('minehound.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),   
+   
+    url(r'^admin/', include(admin.site.urls)),
 )
