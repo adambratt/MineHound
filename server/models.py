9663ad0a7c4108e9e0bfebac290dc4ea269ac387
Adam Bratt <adamjbratt@gmail.com>
2012-03-24 19:18:23 -0400

    boom

--- server/models.py
+++ server/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
-# Create your models here.
 
-#class 
+
+class user(models.Model):
+    ''' A Minecraft user '''
+    username = models.CharField(max_length=50)
+
+class server(models.Model):
+    ''' A Minecraft server '''
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    server_port = models.IntegerField(default=25565)
+    query_port = models.IntegerField(default=25566)
+    last_checked = models.DateTimeField()
+    
+
+class session(models.Model):
+    ''' Individual sessions for minecraft users'''
+    user = models.ForeignKey('server.user')
+    server = models.ForeignKey('server.server')
+    length = models.IntegerField(default=0)
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(auto_now=True)
