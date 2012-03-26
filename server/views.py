8a3f1976967653d3a84f42d83f4f38f4305d1f1c
Adam Bratt <adamjbratt@gmail.com>
2012-03-26 01:45:06 -0400

    forgot to import

--- server/views.py
+++ server/views.py
@@ -3,7 +3,7 @@ from server.models import server
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    s = server.objects.all()[:30]
+    s = server.objects.all().order_by('-today_sessions')[:30]
     return render(request, 'server_list.html', {'servers': s})
 
 @login_required
@@ -12,7 +12,7 @@ def create(request):
     
 def view(request, server_id):
     try:
-        s = server.objects.get(pk=server_id).order_by('-today_sessions')
+        s = server.objects.get(pk=server_id)
     except server.DoesNotExist:
         return 
     return render(request, 'server_view.html', {'server': s})
