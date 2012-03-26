6a5850c7d5b65714b39e8eb0da07c16c73d84baf
Adam Bratt <adamjbratt@gmail.com>
2012-03-26 00:51:46 -0400

    changed home template

--- server/views.py
+++ server/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from server.models import server
 
 def home(request):
-    s = Server.objects.all()[:30]
+    s = server.objects.all()[:30]
     return render(request, 'server_list.html', {'servers': s})
