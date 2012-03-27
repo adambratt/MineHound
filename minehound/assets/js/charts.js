c8115ccc724294fe1ec5ba2bd989532e99fbbe34
Adam Bratt <adamjbratt@gmail.com>
2012-03-26 20:37:38 -0400

    added stuff

--- minehound/assets/js/charts.js
+++ minehound/assets/js/charts.js
@@ -5,11 +5,9 @@ function graphHourlyUsers(ele_id, d) {
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Hour');
   data.addColumn('number', 'Users');
-  data.addRows(d.length);
   
   for(x in d){
-    data.setCell(x, 0, (d[x].hour).toString());
-    data.setCell(x, 1, d[x].users);
+    data.addRow((d[x].hour).toString(), d[x].users);
   }
   
   new google.visualization.ImageAreaChart(document.getElementById(ele_id)).draw(data, null);
