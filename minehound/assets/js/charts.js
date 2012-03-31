ef15c66f039ba0acc2d1a65e3b941d05a88da09f
Adam Bratt <adamjbratt@gmail.com>
2012-03-31 17:23:35 -0400

    added form

--- minehound/assets/js/charts.js
+++ minehound/assets/js/charts.js
@@ -11,7 +11,7 @@ function graphHourlyUsers(ele_id, d) {
     data.addRow([(d[x].hour).toString(), d[x].users]);
   }
   
-  new google.visualization.AreaChart(document.getElementById(ele_id)).draw(data, {legend: 'none', chartArea: {left: 20, top: 20, width: "80%", height: "70%"}, pointSize: 5, hAxis: {fontSize: 8, maxAlternation: 1, slantedText: false}, colors:['#9aea5a','#333'], areaOpacity: 0.6});
+  new google.visualization.AreaChart(document.getElementById(ele_id)).draw(data, {legend: 'none', pointSize: 5, hAxis: {fontSize: 8, maxAlternation: 1, slantedText: false}, colors:['#9aea5a','#333'], areaOpacity: 0.6});
     
 }
 
@@ -32,5 +32,5 @@ function graphVisitorPercentage(ele_id, d, title){
     data.addRow(['new', d[0]['new']]);
     data.addRow(['returning', d[0]['returning']]);
     
-    new google.visualization.PieChart(document.getElementById(ele_id)).draw(data, {legend: 'none', chartArea: {left: 20, top: 20, width: "90%", height: "90%"}, colors:['#9aea5a','#333']});
+    new google.visualization.PieChart(document.getElementById(ele_id)).draw(data, {legend: 'none', chartArea: {left: 20, top: 20, width: "90%", height: "80%"}, colors:['#9aea5a','#333']});
 }
